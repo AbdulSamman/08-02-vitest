5a713ec754f015484d6abe2cdb23e3d95c63de19
Your Name <you@example.com>
2023-02-08 11:26:01 +0100

    ready for the test

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import * as config from "./config.js";
+// TODO find a way placethe Vistest tests in /src, currently results in tsconfig.json errors
 app.listen(config.port, () => {
     console.log(`listening on port http://localhost:${config.port}`);
 });
---
+++ server.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "vitest";
+import request from "supertest";
+import app from "./src/app.js";
+
+test(`if the base route works as expected`, async () => {
+  const response = await request(app).get("/");
+  expect(response.statusCode).toBe(200);
+});
