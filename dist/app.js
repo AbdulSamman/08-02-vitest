55f43a3b35b6ba21582ead5e801369e74d686255
Your Name <you@example.com>
2023-02-08 10:10:42 +0100

    app removed from server

---
+++ dist/app.js
@@ -0,0 +1,34 @@
+import * as model from "./model.js";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send(model.getApiInstructions());
+});
+app.get("/books", (req, res) => {
+    res.json(model.getBooks());
+});
+app.get("/books/:id", (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+        res.status(400).send({
+            error: true,
+            message: "sent string, should be number",
+        });
+    }
+    else {
+        const book = model.getBook(id);
+        if (book === undefined) {
+            res.status(404).send({
+                error: true,
+                message: "id did not correspond to an existing item",
+            });
+        }
+        else {
+            res.json(book);
+        }
+    }
+});
+export default app;
+//# sourceMappingURL=app.js.map
